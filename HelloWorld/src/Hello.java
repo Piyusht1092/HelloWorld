3c34fa43f6b19b32fd8358c3d0f9f0c5ff5810c4
piyush <piyush@piyush-PC>
2018-03-16 08:58:21 +0530

    first commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class Hello {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Scanner input  = new Scanner (new File(args[0]));
+		System.out.println("1st program of my life");
+		while (input.hasNextLine()) {
+		    System.out.println(input.nextLine());
+		}
+	}
+
+}
