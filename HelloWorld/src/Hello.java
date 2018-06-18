564d9b269ac9abcacbb87c9b26c41a46a43c2f0a
Piyusht1092 <piyusht1092@gmail.com>
2018-06-18 08:26:31 +0530

    Trying

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		Scanner input  = new Scanner (new File(args[0]));
-		System.out.println("1st program of my life");
+		System.out.println("1st program of my life Piyush");
 		while (input.hasNextLine()) {
 		    System.out.println(input.nextLine());
 		}
